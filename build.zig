00fd431ab531c92c283935b68725cec352512c4a
Søren Gregersen <s.gregersen@gmail.com>
2025-02-05 07:58:29 +0100

    Add support for command-line arguments in build script

--- build.zig
+++ build.zig
@@ -23,6 +23,9 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
