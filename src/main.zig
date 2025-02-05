83d1bf89a9e590c0cae506a33efd6f4060a3fb36
Søren Gregersen <s.gregersen@gmail.com>
2025-02-05 07:58:22 +0100

    Refactor main function to handle command-line arguments and improve usage instructions

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,24 @@
 const std = @import("std");
 const tokens = @import("tokens.zig");
+const vm = @import("virtual_machine.zig");
 
-pub fn main() !void {
-    std.debug.print("Hello World!\n", .{});
+pub fn main() u8 {
+    const allocator = std.heap.page_allocator;
+    const args = std.process.argsAlloc(allocator) catch {
+        return 71; // EX_OSERR
+    };
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len == 1) {
+        std.debug.print("repl();\n", .{}); // repl();
+    } else if (args.len == 2) {
+        std.debug.print("runFile(\"{s}\");\n", .{args[1]}); // runFile(args[1]);
+    } else {
+        const stderr = std.io.getStdErr();
+        stderr.writeAll("Usage: orsum [file]\n") catch {};
+        return 64; // EX_USAGE
+    }
+
+    return 0;
+    // See exit codes at https://man.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
 }
